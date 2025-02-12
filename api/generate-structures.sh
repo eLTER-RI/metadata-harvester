48b6c8bb3087771ef32eee9bbcaf7af349ca7dac
Anna Vítová <avitova@redhat.com>
2025-02-12 11:57:10 +0100

    src: add b2share api generation

--- api/generate-structures.sh
+++ api/generate-structures.sh
@@ -2,4 +2,10 @@
 
 npx @rtk-query/codegen-openapi api/config/deimsApi.ts
 
+input_file="api/schema/b2share-schema.json"
+output_file="api/schema/b2share-extracted-schema.json"
+jq '.json_schema' "$input_file" > "$output_file"
+echo "json_schema extracted and saved as $output_file"
+json2ts api/schema/b2share-extracted-schema.json -o ./src/store/b2shareApi.ts
+
 wait
