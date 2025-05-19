05c482b441b51a6e737196786ac12f1d41176fd3
Anna Vítová <anickavitova@seznam.cz>
2025-05-19 13:20:51 +0200

    feat: add config with variables

---
+++ config.ts
@@ -0,0 +1,8 @@
+export const CONFIG = {
+    INITIAL_API_URL:
+      'https://b2share.eudat.eu/api/records/?q=community:e9b9792e-79fb-4b07-b6b4-b9c2bd06d095&sort=oldest',
+    DEIMS_API_URL: 'https://deims.org/api/sites',
+    OUTPUT_FILE: 'mapped_records.json',
+    PAGE_SIZE: 100,
+  };
+  
