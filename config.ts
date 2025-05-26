fa20e6c65ce856a3b4ab97fe4f76d3e0dbc384a9
Anna Vítová <anickavitova@seznam.cz>
2025-05-26 20:26:01 +0200

    refactor: apply eslint rule to config

--- config.ts
+++ config.ts
@@ -1,8 +1,6 @@
 export const CONFIG = {
-    INITIAL_API_URL:
-      'https://b2share.eudat.eu/api/records/?q=community:e9b9792e-79fb-4b07-b6b4-b9c2bd06d095&sort=oldest',
-    DEIMS_API_URL: 'https://deims.org/api/sites',
-    OUTPUT_FILE: 'mapped_records.json',
-    PAGE_SIZE: 100,
-  };
-  
+  INITIAL_API_URL: 'https://b2share.eudat.eu/api/records/?q=community:d952913c-451e-4b5c-817e-d578dc8a4469&sort=oldest',
+  DEIMS_API_URL: 'https://deims.org/api/sites',
+  OUTPUT_FILE: 'mapped_records.json',
+  PAGE_SIZE: 100,
+};
