a58192caceaec3b24310023b7a03943009ca2894
Anna Vítová <anickavitova@seznam.cz>
2025-06-02 12:35:49 +0200

    fix: update pull schema to pull correct community

--- api/pull-schema.sh
+++ api/pull-schema.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl https://b2share.eudat.eu/api/communities/e9b9792e-79fb-4b07-b6b4-b9c2bd06d095/schemas/2#/json_schema -o api/schema/b2share-schema.json
+curl https://b2share.eudat.eu/api/communities/d952913c-451e-4b5c-817e-d578dc8a4469/schemas/2#/json_schema -o api/schema/b2share-schema.json
 curl https://deims.org/api | jq . > api/schema/deims-schema.json
