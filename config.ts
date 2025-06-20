4acee996e4281d70fde804144f2e3ad00b0b6c25
Anna Vítová <anickavitova@seznam.cz>
2025-06-20 14:46:21 +0200

    feat: add sites constants

--- config.ts
+++ config.ts
@@ -1,6 +1,8 @@
 export const CONFIG = {
   B2SHARE_API_URL: 'https://b2share.eudat.eu/api/records/?q=community:d952913c-451e-4b5c-817e-d578dc8a4469&sort=oldest',
+  SITES_API_URL: 'https://meta.fieldsites.se/sparql',
   DEIMS_API_URL: 'https://deims.org/api/sites',
-  B2SHARE_MAPPED_RECORDS: './data/mapped_records.json',
+  B2SHARE_MAPPED_RECORDS: './data/mapped_b2share_records.json',
+  SITES_MAPPED_RECORDS: 'mapped_sites_records.json',
   PAGE_SIZE: 100,
 };
