6830486d82fdd9281d965a11c136b96b9b44e101
Anna Vítová <anickavitova@seznam.cz>
2025-08-01 12:08:07 +0200

    feat: implement retry mechanism for fetching records

--- src/services/pullAllData.ts
+++ src/services/pullAllData.ts
@@ -9,8 +9,11 @@ import { RepositoryType } from '../store/commonStructure';
 import { mapZenodoToCommonDatasetMetadata } from '../store/zenodoParser';
 import { RateLimiter } from './rateLimiter';
 
+const MAX_RETRIES = 5;
+const INITIAL_RETRY_DELAY_MS = 20000;
+
 // Function to fetch JSON from a URL
-export async function fetchJson(url: string): Promise<any> {
+export async function fetchJson(url: string, retriesLeft = MAX_RETRIES, delay = INITIAL_RETRY_DELAY_MS): Promise<any> {
   try {
     await new Promise((resolve) => setTimeout(resolve, 500));
     const response = await fetch(url, {
@@ -19,6 +22,13 @@ export async function fetchJson(url: string): Promise<any> {
       },
     });
     if (!response.ok) {
+      if (response.status === 429 && retriesLeft > 0) {
+        process.stderr.write(`Received 429 Too Many Requests for ${url}. Retrying...\n`);
+        const retryAfter = 10000;
+        await new Promise((resolve) => setTimeout(resolve, retryAfter));
+        return fetchJson(url, retriesLeft - 1, delay * 2);
+      }
+
       let errorBody = 'No response body';
       try {
         const contentType = response.headers.get('content-type');
@@ -32,7 +42,7 @@ export async function fetchJson(url: string): Promise<any> {
       }
 
       const errorMessage = `
-        Error fetching ${url}: Request faile with status: ${response.status} ${response.statusText}
+        Error fetching ${url}: Request failed with status: ${response.status} ${response.statusText}
         URL: ${response.url}
         Response Body: ${errorBody}
       `;
