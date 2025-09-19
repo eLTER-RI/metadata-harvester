cab2fd1bdce032bc720656bef0f97e206af8a15d
Anna Vítová <anickavitova@seznam.cz>
2025-09-19 14:48:04 +0200

    docs: remove author's comment

--- src/services/jobs/harvest/dbValidation.ts
+++ src/services/jobs/harvest/dbValidation.ts
@@ -2,13 +2,6 @@ import { log } from './../../serviceLogging';
 import { HarvesterContext } from './harvester';
 import { fieldSitesLimiter, zenodoLimiter } from './../../rateLimiterConcurrency';
 
-// for example, for zenodo, we can find out if it "is_last" - if it is, just upsert and put
-// if it's not, fetch versions, get latest, and run it for it
-
-// we can even just set all checksums to null, change the source_url, keep dar_id, and that's it
-
-// obalit tohle cely do transakce
-
 /**
  * This function validates the records present in the local database against the source.
  * It checks if the source URLs are still valid and if the source data has changed by comparing checksums.
