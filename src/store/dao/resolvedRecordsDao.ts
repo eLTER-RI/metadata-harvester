25aebc44d081b8dd12f163600990b1d723bd0715
Anna Vítová <anickavitova@seznam.cz>
2025-09-22 19:50:57 +0200

    feat: add resolved creation to dao

--- src/store/dao/resolvedRecordsDao.ts
+++ src/store/dao/resolvedRecordsDao.ts
@@ -13,4 +13,13 @@ export class ResolvedRecordDao {
   constructor(pool: Pool) {
     this.pool = pool;
   }
+
+  async create(record: Omit<DbResolvedRecord, 'last_harvested'>): Promise<void> {
+    const query = `
+      INSERT INTO resolved_records (dar_id, resolved_by, resolved_at)
+      VALUES ($1, $2, NOW());
+    `;
+    const values = [record.dar_id, record.resolved_by];
+    await this.pool.query(query, values);
+  }
 }
