d629df04788110f5e9f36055e0f0a946d36b956c
Anna Vítová <anickavitova@seznam.cz>
2025-09-23 09:59:48 +0200

    feat: add necessary resolved_records dao queries

--- src/store/dao/resolvedRecordsDao.ts
+++ src/store/dao/resolvedRecordsDao.ts
@@ -1,4 +1,5 @@
-import { Pool } from 'pg';
+import { Pool, QueryResult } from 'pg';
+import { DbRecord } from './recordDao';
 
 export interface DbResolvedRecord {
   id: number;
@@ -21,5 +22,34 @@ export class ResolvedRecordDao {
     `;
     const values = [record.dar_id, record.resolved_by];
     await this.pool.query(query, values);
+  }
+
+  async delete(dar_id: string): Promise<void> {
+    const query = `
+      DELETE FROM resolved_records
+      WHERE dar_id = $1
+    `;
+    await this.pool.query(query, [dar_id]);
+  }
+
+  async getAllResolved(): Promise<DbRecord[]> {
+    const query = `
+      SELECT r.*
+      FROM harvested_records h
+      INNER JOIN resolved_records r ON h.dar_id = r.dar_id;
+    `;
+    const result: QueryResult<DbRecord> = await this.pool.query(query);
+    return result.rows;
+  }
+
+  async getAllUnresolved(): Promise<DbRecord[]> {
+    const query = `
+      SELECT r.*
+      FROM harvested_records h
+      LEFT JOIN resolved_records r ON h.dar_id = r.dar_id
+      WHERE r.dar_id IS NULL;
+    `;
+    const result: QueryResult<DbRecord> = await this.pool.query(query);
+    return result.rows;
   }
 }
