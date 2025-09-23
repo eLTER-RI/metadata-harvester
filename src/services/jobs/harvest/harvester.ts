4146ac55e9f75f639c0eaba61765d06f0efb2d40
Anna Vítová <anickavitova@seznam.cz>
2025-09-23 10:12:42 +0200

    move record upsert to separate file

--- src/services/jobs/harvest/harvester.ts
+++ src/services/jobs/harvest/harvester.ts
@@ -21,6 +21,7 @@ import { fieldSitesLimiter, zenodoLimiter } from '../../rateLimiterConcurrency';
 import { dbValidationPhase } from './dbValidation';
 import { RuleDao } from '../../../store/dao/rulesDao';
 import { postToDar, putToDar } from '../../../../api/darApi';
+import { dbRecordUpsert } from './dbRecordSync';
 
 // Configurations
 const currentEnv = process.env.NODE_ENV;
@@ -383,78 +384,6 @@ export class HarvesterContext {
       await this.recordDao.updateStatus(sourceUrl, { status: 'failed' });
     }
   }
-}
-
-/**
- * CREATE or UPDATE of a record in the local database based on its existence and status of the synchronization.
- * It handles creating a new record, updating an old version of a record, or simply updating an existing record's status.
- * @param {string | null} darId The ID of the record in DAR. If set to null, it assumes record POST/PUT to dar was unsuccessful.
- * @param {boolean} missingInDb.
- * @param {RecordDao} recordDao
- * @param {string} sourceUrl The source URL of the record on the remote repository.
- * @param {RepositoryType} repositoryType The type of the repository (e.g., 'ZENODO', 'B2SHARE_EUDAT'...).
- * @param {string} sourceChecksum The checksum of the current source data.
- * @param {string} darChecksum Source data checksum.
- * @param {string} oldUrl (Optional) The old URL of the record if there is a new version but we have an older version with different sourceUrl.
- */
-async function dbRecordUpsert(
-  darId: string | null,
-  recordDao: RecordDao,
-  sourceUrl: string,
-  repositoryType: RepositoryType,
-  sourceChecksum: string,
-  darChecksum: string,
-  oldUrl?: string,
-) {
-  const dbMatches = await recordDao.getRecordBySourceId(sourceUrl);
-  const missingInDb = dbMatches.length === 0;
-  if (!darId) {
-    await recordDao.updateDarIdStatus(sourceUrl, {
-      status: 'failed',
-    });
-    return;
-  }
-
-  if (missingInDb) {
-    log('info', `Creating database record for ${sourceUrl}`);
-    await recordDao.createRecord({
-      source_url: sourceUrl,
-      source_repository: repositoryType,
-      source_checksum: sourceChecksum,
-      dar_id: darId,
-      dar_checksum: darChecksum,
-      status: 'success',
-    });
-    return;
-  }
-
-  if (oldUrl) {
-    log('info', `Record ${sourceUrl} has an old version of ${oldUrl} in DAR with ${darId}`);
-    await recordDao.updateRecordWithPrimaryKey(oldUrl, {
-      source_url: sourceUrl,
-      source_repository: repositoryType,
-      source_checksum: sourceChecksum,
-      dar_id: darId,
-      dar_checksum: darChecksum,
-      status: 'updating',
-    });
-    return;
-  }
-
-  log('info', `Updating record in database for record ${sourceUrl}, dar id ${darId}`);
-  await recordDao.updateRecord(sourceUrl, {
-    source_url: sourceUrl,
-    source_repository: repositoryType,
-    source_checksum: sourceChecksum,
-    dar_id: darId,
-    dar_checksum: darChecksum,
-    status: 'updating',
-  });
-
-  log('info', 'Record was up to date.');
-  await recordDao.updateStatus(sourceUrl, {
-    status: 'success',
-  });
 }
 
 /**
