17f8b35852acd30ca71d5b08f7cd831d3f6b3afd
Anna Vítová <anickavitova@seznam.cz>
2025-09-26 12:10:03 +0200

    feat: add prettier and eslint to the frontend repository

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    extends: [...tseslint.configs.recommended],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'max-len': [
+        'error',
+        {
+          code: 120,
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': ['error', { printWidth: 120 }],
+    },
+  },
+  prettierRecommended,
+);
