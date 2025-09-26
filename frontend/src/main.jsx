cffbc8a3d3d5abfbf93ac11e53ae63420f305b23
Anna Vítová <anickavitova@seznam.cz>
2025-09-26 13:02:05 +0200

    frontend: import semantic css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'semantic-ui-css/semantic.min.css';
 import App from './App.jsx'
 
+// createRoot(document.getElementById("ingestRoot")!).render(
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
