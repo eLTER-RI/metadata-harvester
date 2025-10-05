70720df214ac884f1dac49fd371457d7be3098e1
Anna Vítová <anickavitova@seznam.cz>
2025-10-05 21:19:45 +0200

    tests: create and update record

--- tests/store/dao/recordDao.test.ts
+++ tests/store/dao/recordDao.test.ts
@@ -42,4 +42,31 @@ describe('Tests for RecordDao', () => {
     expect(result[0].status).toBe('success');
     expect(result[0].title).toBe('Record for Testing');
   });
+
+  it('updates a record', async () => {
+    const source_url = 'http://repostiory.com/uuid';
+    const newRecord: Omit<DbRecord, 'last_harvested'> = {
+      source_url: source_url,
+      source_repository: 'GITHUB',
+      source_checksum: 'source_checksum123',
+      dar_id: 'uuid-uuid',
+      dar_checksum: 'dar_checksum123',
+      status: 'success',
+      title: 'Record for Testing',
+    };
+
+    await recordDao.createRecord(newRecord);
+    const result = await recordDao.getRecordBySourceId(source_url);
+
+    expect(result).toHaveLength(1);
+    expect(result[0].title).toBe('Record for Testing');
+
+    newRecord.dar_checksum = 'dar_checksum456';
+    newRecord.source_checksum = 'source_checksum456';
+    await recordDao.updateRecord(source_url, newRecord);
+    const updated = await recordDao.getRecordBySourceId(source_url);
+    expect(updated[0].dar_checksum).toBe('dar_checksum456');
+    expect(updated[0].source_checksum).toBe('source_checksum456');
+    expect(updated[0].status).toBe('success');
+  });
 });
