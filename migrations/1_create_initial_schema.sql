03e0f7199068bbae33de8f711b6c87abbe7edec4
Anna Vítová <anickavitova@seznam.cz>
2025-10-05 12:20:34 +0200

    db: add migration mechanism and add testing script
    In the future, this project will need handling the database with migrations. This commit remove the previous mechanism that used the script, and implements migrations.

---
+++ migrations/1_create_initial_schema.sql
@@ -0,0 +1,37 @@
+-- Up Migration
+CREATE TABLE IF NOT EXISTS harvested_records (
+  source_url TEXT PRIMARY KEY,
+  source_repository TEXT NOT NULL,
+  source_checksum TEXT NOT NULL,
+  dar_id TEXT UNIQUE,
+  dar_checksum TEXT,
+  status TEXT,
+  last_harvested TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  title TEXT
+);
+
+CREATE TABLE IF NOT EXISTS deims_sites (
+  id TEXT PRIMARY KEY,
+  name TEXT NOT NULL,
+  shortname TEXT,
+  site_data JSONB NOT NULL,
+  checksum TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS record_rules (
+  id SERIAL PRIMARY KEY,
+  dar_id TEXT NOT NULL,
+  rule_type VARCHAR(50) NOT NULL,
+  target_path TEXT NOT NULL,
+  orig_value JSONB NOT NULL,
+  new_value JSONB NULL,
+  UNIQUE (dar_id, target_path)
+);
+
+CREATE TABLE IF NOT EXISTS resolved_records (
+  id SERIAL PRIMARY KEY,
+  dar_id TEXT UNIQUE NOT NULL,
+  resolved_by TEXT,
+  resolved_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
