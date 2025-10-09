f4d34f55da9b3f209cabef93f2b0bed93cdd72ba
Anna Vítová <anickavitova@seznam.cz>
2025-10-09 11:43:15 +0200

    tests: harvester context creation and calling mappers

--- tests/services/jobs/harvest/harvester.test.ts
+++ tests/services/jobs/harvest/harvester.test.ts
@@ -1,7 +1,12 @@
 import { Pool } from 'pg';
+import { HarvesterContext } from '../../../../src/services/jobs/harvest/harvester';
 import { RecordDao } from '../../../../src/store/dao/recordDao';
 import { ResolvedRecordDao } from '../../../../src/store/dao/resolvedRecordsDao';
 import { fetchSites } from '../../../../src/utilities/matchDeimsId';
+import { mapB2ShareToCommonDatasetMetadata } from '../../../../src/store/parsers/b2shareParser';
+import { mapDataRegistryToCommonDatasetMetadata } from '../../../../src/store/parsers/dataregistryParser';
+import { mapZenodoToCommonDatasetMetadata } from '../../../../src/store/parsers/zenodoParser';
+import { mapFieldSitesToCommonDatasetMetadata } from '../../../../src/store/parsers/fieldSitesParser';
 
 // To isolate all other layers, we need to isolate the following:
 // those should be tested separately
@@ -46,5 +51,64 @@ describe('Test harvester file', () => {
     mockResolvedRecordsDao.delete.mockResolvedValue(undefined);
 
     mockedFetchSites.mockResolvedValue([{ siteID: 'deims-1', siteName: 'Test Site' }]);
+  });
+
+  describe('Tests HarvesterContext', () => {
+    it('should have correct properties based on repository types', async () => {
+      const zenodoContext = await HarvesterContext.create(mockPool, 'ZENODO', true);
+      expect(fetchSites).toHaveBeenCalled();
+      expect(zenodoContext.repositoryType).toBe('ZENODO');
+      expect(zenodoContext.sites).toHaveLength(1);
+      expect(zenodoContext.repoConfig.processFunction).toBe('processApiPage');
+      expect(zenodoContext.repoConfig.selfLinkKey).toBe('links.self');
+      expect(zenodoContext.repoConfig.dataKey).toBe('hits.hits');
+
+      const b2shareContext = await HarvesterContext.create(mockPool, 'B2SHARE_EUDAT', true);
+      expect(fetchSites).toHaveBeenCalled();
+      expect(b2shareContext.repositoryType).toBe('B2SHARE_EUDAT');
+      expect(b2shareContext.sites).toHaveLength(1);
+      expect(b2shareContext.repoConfig.processFunction).toBe('processApiPage');
+      expect(b2shareContext.repoConfig.selfLinkKey).toBe('links.self');
+      expect(b2shareContext.repoConfig.dataKey).toBe('hits.hits');
+
+      const dataregistryContext = await HarvesterContext.create(mockPool, 'DATAREGISTRY', true);
+      expect(fetchSites).toHaveBeenCalled();
+      expect(dataregistryContext.repositoryType).toBe('DATAREGISTRY');
+      expect(dataregistryContext.sites).toHaveLength(1);
+      expect(dataregistryContext.repoConfig.processFunction).toBe('processApiPage');
+      expect(dataregistryContext.repoConfig.selfLinkKey).toBe('link');
+      expect(dataregistryContext.repoConfig.dataKey).toBe('resources');
+      expect(dataregistryContext.repoConfig.singleRecordKey).toBe('resource');
+
+      const fieldSitesContext = await HarvesterContext.create(mockPool, 'SITES', true);
+      expect(fetchSites).toHaveBeenCalled();
+      expect(fieldSitesContext.repositoryType).toBe('SITES');
+      expect(fieldSitesContext.sites).toHaveLength(1);
+      expect(fieldSitesContext.repoConfig.processFunction).toBe('processFieldSitesPage');
+      expect(fieldSitesContext.repoConfig.selfLinkKey).toBeUndefined();
+      expect(fieldSitesContext.repoConfig.dataKey).toBeUndefined();
+    });
+
+    it('should call the correct mapper based on repositoryType', async () => {
+      (mapB2ShareToCommonDatasetMetadata as jest.Mock).mockResolvedValue({});
+      (mapZenodoToCommonDatasetMetadata as jest.Mock).mockResolvedValue({});
+      (mapDataRegistryToCommonDatasetMetadata as jest.Mock).mockResolvedValue({});
+      (mapFieldSitesToCommonDatasetMetadata as jest.Mock).mockResolvedValue({});
+
+      const b2shareRecord = { id: 'b2share1' };
+      const b2shareContext = await HarvesterContext.create(mockPool, 'B2SHARE_EUDAT', true);
+      await b2shareContext.mapToCommonStructure('http://example.com/b2share/1', b2shareRecord);
+      expect(mapB2ShareToCommonDatasetMetadata).toHaveBeenCalled();
+
+      const zenodoRecord = { id: 'zenodo1' };
+      const zenodoContext = await HarvesterContext.create(mockPool, 'ZENODO', true);
+      await zenodoContext.mapToCommonStructure('http://example.com/zenodo/1', zenodoRecord);
+      expect(mapZenodoToCommonDatasetMetadata).toHaveBeenCalled();
+
+      const dataregistryRecord = { id: 'dataregistry1' };
+      const dataregistryContext = await HarvesterContext.create(mockPool, 'DATAREGISTRY', true);
+      await dataregistryContext.mapToCommonStructure('http://example.com/dataregistry/1', dataregistryRecord);
+      expect(mapDataRegistryToCommonDatasetMetadata).toHaveBeenCalled();
+    });
   });
 });
