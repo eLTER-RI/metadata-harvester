6be22809afaf92389363f398a77280b779ea42f4
Anna Vítová <anickavitova@seznam.cz>
2025-10-09 23:18:00 +0200

    tests: rules set nested value

--- tests/src/utilities/rules.test.ts
+++ tests/src/utilities/rules.test.ts
@@ -1,4 +1,4 @@
-import { getNestedValue } from '../../../src/utilities/rules';
+import { getNestedValue, setNestedValue } from '../../../src/utilities/rules';
 
 describe('Rules Utility Functions', () => {
   let testObj: any;
@@ -61,6 +61,59 @@ describe('Rules Utility Functions', () => {
         'The lake that is a subject of measurements is located in Finland.',
       );
       expect(getNestedValue(testObj, 'metadata.descriptions[1].type')).toBe('description');
+    });
+  });
+
+  describe('setNestedValue', () => {
+    it('should replace not-nested simple type value', () => {
+      setNestedValue(testObj, 'organisation', 'Turku Forskningsgrupp');
+      expect(testObj.organisation).toBe('Turku Forskningsgrupp');
+    });
+
+    it('should replace not-nested array and single value', () => {
+      setNestedValue(testObj, 'versions', ['aaaaaaaa', 'uuuuu']);
+      expect(testObj.versions).toEqual(['aaaaaaaa', 'uuuuu']);
+      expect(setNestedValue(testObj, 'versions[0]', 'abcdefg'));
+      expect(testObj.versions[0]).toEqual('abcdefg');
+    });
+
+    it('should replace nested simple type value', () => {
+      setNestedValue(testObj, 'metadata.created_at', '2023-10-09');
+      expect(testObj.metadata.created_at).toBe('2023-10-09');
+    });
+
+    it('should replace nested structure', () => {
+      setNestedValue(testObj, 'metadata.creator', {
+        creatorFirstName: 'Tommi',
+        creatorLastName: 'Ranskalainen',
+      });
+      expect(testObj.metadata.creator).toEqual({
+        creatorFirstName: 'Tommi',
+        creatorLastName: 'Ranskalainen',
+      });
+      setNestedValue(testObj, 'metadata.creator.creatorFirstName', 'Aino');
+      setNestedValue(testObj, 'metadata.creator.creatorLastName', 'Korhonen');
+      expect(testObj.metadata.creator).toEqual({
+        creatorFirstName: 'Aino',
+        creatorLastName: 'Korhonen',
+      });
+    });
+
+    it('should replace values from a nested array', () => {
+      setNestedValue(testObj, 'metadata.descriptions', [{ type: 'different type', value: 'new value' }]);
+      expect(testObj.metadata.descriptions).toEqual([{ type: 'different type', value: 'new value' }]);
+      setNestedValue(testObj, 'metadata.descriptions[0]', {
+        type: 'aaaaa',
+        value: 'bbbb.',
+      });
+      expect(testObj.metadata.descriptions[0]).toEqual({
+        type: 'aaaaa',
+        value: 'bbbb.',
+      });
+      setNestedValue(testObj, 'metadata.descriptions[0].value', 'first description value');
+      expect(testObj.metadata.descriptions[0].value).toBe('first description value');
+      setNestedValue(testObj, 'metadata.descriptions[0].type', 'first description type');
+      expect(testObj.metadata.descriptions[0].type).toBe('first description type');
     });
   });
 });
