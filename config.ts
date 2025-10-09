f5954b780f4c5547f85aed677010a84518828db3
Anna Vítová <anickavitova@seznam.cz>
2025-10-09 11:04:15 +0200

    fix: remove deprecated config properties

--- config.ts
+++ config.ts
@@ -1,6 +1,5 @@
 export interface RepositoryConfig {
   apiUrl: string;
-  mappedRecordsPath: string;
   pageSize?: number;
   selfLinkKey?: string;
   dataKey?: string;
@@ -13,54 +12,44 @@ export const CONFIG = {
   REPOSITORIES: {
     B2SHARE_EUDAT: {
       apiUrl: 'https://b2share.eudat.eu/api/records/?q=community:d952913c-451e-4b5c-817e-d578dc8a4469&sort=oldest',
-      mappedRecordsPath: './data/mapped_b2share_eudat_records.json',
       pageSize: 100,
       selfLinkKey: 'links.self',
       dataKey: 'hits.hits',
       processFunction: 'processApiPage',
-      rateLimit: 0,
       darQuery:
         'https://dar.elter-ri.eu/api/external-datasets/?q=&metadata_projects_projectName=B2SHARE%20Eudat%20external%20record%20-%20eLTER%20Community&sort=newest&size=300',
     } as RepositoryConfig,
     B2SHARE_JUELICH: {
       apiUrl: 'https://b2share.fz-juelich.de/api/records/?q=community:d952913c-451e-4b5c-817e-d578dc8a4469&sort=oldest',
-      mappedRecordsPath: './data/mapped_b2share_juelich_records.json',
       pageSize: 100,
       selfLinkKey: 'links.self',
       dataKey: 'hits.hits',
       processFunction: 'processApiPage',
-      rateLimit: 0,
       darQuery:
         'https://dar.elter-ri.eu/api/external-datasets/?q=&metadata_projects_projectName=B2SHARE%20Juelich%20external%20record%20-%20eLTER%20Community&sort=newest&size=100',
     } as RepositoryConfig,
     DATAREGISTRY: {
       apiUrl: 'https://dataregistry.lteritalia.it/api/v2/resources?format=json',
-      mappedRecordsPath: './data/mapped_dataregistry_records.json',
       pageSize: 10,
       selfLinkKey: 'link',
       dataKey: 'resources',
       singleRecordKey: 'resource',
       processFunction: 'processApiPage',
-      rateLimit: 0,
       darQuery:
         'https://dar.elter-ri.eu/api/external-datasets/?q=&metadata_projects_projectName=Dataregistry%20LTER-Italy%20DAR&sort=newest&size=100',
     },
     SITES: {
       apiUrl: 'https://meta.fieldsites.se/data-sitemap.xml',
-      mappedRecordsPath: './data/mapped_sites_records.json',
       processFunction: 'processFieldSitesPage',
-      rateLimit: 0,
       darQuery:
         'https://dar.elter-ri.eu/api/external-datasets/?q=&metadata_externalSourceInformation_externalSourceName=FieldSites&size=1500',
     } as RepositoryConfig,
     ZENODO: {
       apiUrl: 'https://zenodo.org/api/communities/c4f49aa1-a5ef-46b1-a7ec-7756cd391aa4/records?q=&sort=oldest',
-      mappedRecordsPath: './data/mapped_zenodo_records.json',
       pageSize: 100,
       dataKey: 'hits.hits',
       selfLinkKey: 'links.self',
       processFunction: 'processApiPage',
-      rateLimit: 100,
       darQuery:
         'https://dar.elter-ri.eu/api/external-datasets/?q=&metadata_externalSourceInformation_externalSourceName=Zenodo&sort=newest&size=500',
     } as RepositoryConfig,
@@ -71,7 +60,6 @@ export const CONFIG = {
       dataKey: 'hits.hits',
       selfLinkKey: 'links.self',
       processFunction: 'processApiPage',
-      rateLimit: 100,
       darQuery:
         'https://dar.elter-ri.eu/api/external-datasets/?q=&metadata_externalSourceInformation_externalSourceName=Zenodo&sort=newest&size=500',
     } as RepositoryConfig,
