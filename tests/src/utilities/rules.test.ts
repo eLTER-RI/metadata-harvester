446cc908c5af5a113c4b85039f2bc112c8783f5f
Anna Vítová <anickavitova@seznam.cz>
2025-10-13 10:27:02 +0200

    tests: fix and add more tests for rules

--- tests/src/utilities/rules.test.ts
+++ tests/src/utilities/rules.test.ts
@@ -1,119 +1,174 @@
-import { getNestedValue, setNestedValue } from '../../../src/utilities/rules';
+import { CommonDataset } from '../../../src/store/commonStructure';
+import { appendValue, getNestedValue, setNestedValue } from '../../../src/utilities/rules';
 
 describe('Rules Utility Functions', () => {
-  let testObj: any;
+  let sampleRecord: CommonDataset;
 
   beforeEach(() => {
-    testObj = {
+    sampleRecord = {
       metadata: {
-        created_at: '2025-10-09-22:30:00',
+        assetType: 'DOI',
+        publicationDate: '2025-10-09-22:30:00',
         descriptions: [
-          { type: 'abstract', value: 'This file gathers data about the oxygen levels in lake Kivesjärvi.' },
-          { type: 'description', value: 'The lake that is a subject of measurements is located in Finland.' },
+          {
+            descriptionType: 'abstract',
+            descriptionText: 'This file gathers data about the oxygen levels in lake Kivesjärvi.',
+          },
+          {
+            descriptionType: 'description',
+            descriptionText: 'The lake that is a subject of measurements is located in Finland.',
+          },
         ],
-        creator: {
-          creatorFirstName: 'Lauri',
-          creatorLastName: 'Suomalainen',
+        creators: [
+          {
+            creatorGivenName: 'Lauri',
+            creatorFamilyName: 'Suomalainen',
+          },
+        ],
+        dataLevel: {
+          dataLevelCode: '1',
+          dataLevelURI: 'someuri',
         },
+        datasetType: 'Dataset',
+        taxonomicCoverages: ['first', 'second', 'third'],
+        externalSourceInformation: {},
       },
-      versions: ['a1b2c3d4', 'z9y8x7w6'],
-      organisation: 'Åbo Forskningsgrupp',
     };
   });
 
   describe('getNestedValue', () => {
-    it('should find not-nested simple type value', () => {
-      expect(getNestedValue(testObj, 'organisation')).toBe('Åbo Forskningsgrupp');
-    });
-
-    it('should find not-nested array and single value', () => {
-      expect(getNestedValue(testObj, 'versions')).toEqual(['a1b2c3d4', 'z9y8x7w6']);
-      expect(getNestedValue(testObj, 'versions[0]')).toBe('a1b2c3d4');
-    });
-
     it('should find nested simple type value', () => {
-      expect(getNestedValue(testObj, 'metadata.created_at')).toBe('2025-10-09-22:30:00');
+      expect(getNestedValue(sampleRecord, 'metadata.datasetType')).toBe('Dataset');
+    });
+
+    it('should find nested array and single value in it', () => {
+      expect(getNestedValue(sampleRecord, 'metadata.taxonomicCoverages')).toEqual(['first', 'second', 'third']);
+      expect(getNestedValue(sampleRecord, 'metadata.taxonomicCoverages[0]')).toBe('first');
     });
 
     it('should find nested structure', () => {
-      expect(getNestedValue(testObj, 'metadata.creator')).toEqual({
-        creatorFirstName: 'Lauri',
-        creatorLastName: 'Suomalainen',
+      expect(getNestedValue(sampleRecord, 'metadata.dataLevel')).toEqual({
+        dataLevelCode: '1',
+        dataLevelURI: 'someuri',
       });
-      expect(getNestedValue(testObj, 'metadata.creator.creatorFirstName')).toBe('Lauri');
-      expect(getNestedValue(testObj, 'metadata.creator.creatorLastName')).toBe('Suomalainen');
+      expect(getNestedValue(sampleRecord, 'metadata.dataLevel.dataLevelCode')).toBe('1');
+      expect(getNestedValue(sampleRecord, 'metadata.dataLevel.dataLevelURI')).toBe('someuri');
     });
 
     it('should get values from a nested array', () => {
-      expect(getNestedValue(testObj, 'metadata.descriptions')).toEqual([
-        { type: 'abstract', value: 'This file gathers data about the oxygen levels in lake Kivesjärvi.' },
-        { type: 'description', value: 'The lake that is a subject of measurements is located in Finland.' },
+      expect(getNestedValue(sampleRecord, 'metadata.descriptions')).toEqual([
+        {
+          descriptionType: 'abstract',
+          descriptionText: 'This file gathers data about the oxygen levels in lake Kivesjärvi.',
+        },
+        {
+          descriptionType: 'description',
+          descriptionText: 'The lake that is a subject of measurements is located in Finland.',
+        },
       ]);
-      expect(getNestedValue(testObj, 'metadata.descriptions[0]')).toEqual({
-        type: 'abstract',
-        value: 'This file gathers data about the oxygen levels in lake Kivesjärvi.',
+      expect(getNestedValue(sampleRecord, 'metadata.descriptions[0]')).toEqual({
+        descriptionType: 'abstract',
+        descriptionText: 'This file gathers data about the oxygen levels in lake Kivesjärvi.',
       });
-      expect(getNestedValue(testObj, 'metadata.descriptions[0].value')).toBe(
+      expect(getNestedValue(sampleRecord, 'metadata.descriptions[0].descriptionText')).toBe(
         'This file gathers data about the oxygen levels in lake Kivesjärvi.',
       );
-      expect(getNestedValue(testObj, 'metadata.descriptions[0].type')).toBe('abstract');
-      expect(getNestedValue(testObj, 'metadata.descriptions[1].value')).toBe(
+      expect(getNestedValue(sampleRecord, 'metadata.descriptions[0].descriptionType')).toBe('abstract');
+      expect(getNestedValue(sampleRecord, 'metadata.descriptions[1].descriptionText')).toBe(
         'The lake that is a subject of measurements is located in Finland.',
       );
-      expect(getNestedValue(testObj, 'metadata.descriptions[1].type')).toBe('description');
+      expect(getNestedValue(sampleRecord, 'metadata.descriptions[1].descriptionType')).toBe('description');
     });
   });
 
   describe('setNestedValue', () => {
-    it('should replace not-nested simple type value', () => {
-      setNestedValue(testObj, 'organisation', 'Turku Forskningsgrupp');
-      expect(testObj.organisation).toBe('Turku Forskningsgrupp');
-    });
-
-    it('should replace not-nested array and single value', () => {
-      setNestedValue(testObj, 'versions', ['aaaaaaaa', 'uuuuu']);
-      expect(testObj.versions).toEqual(['aaaaaaaa', 'uuuuu']);
-      expect(setNestedValue(testObj, 'versions[0]', 'abcdefg'));
-      expect(testObj.versions[0]).toEqual('abcdefg');
-    });
-
     it('should replace nested simple type value', () => {
-      setNestedValue(testObj, 'metadata.created_at', '2023-10-09');
-      expect(testObj.metadata.created_at).toBe('2023-10-09');
+      setNestedValue(sampleRecord, 'metadata.assetType', '2023-10-09');
+      expect(sampleRecord.metadata.assetType).toBe('2023-10-09');
     });
 
     it('should replace nested structure', () => {
-      setNestedValue(testObj, 'metadata.creator', {
-        creatorFirstName: 'Tommi',
-        creatorLastName: 'Ranskalainen',
+      setNestedValue(sampleRecord, 'metadata.dataLevel', {
+        dataLevelCode: '2',
+        dataLevelURI: 'otheruri',
       });
-      expect(testObj.metadata.creator).toEqual({
-        creatorFirstName: 'Tommi',
-        creatorLastName: 'Ranskalainen',
+      expect(sampleRecord.metadata.dataLevel).toEqual({
+        dataLevelCode: '2',
+        dataLevelURI: 'otheruri',
       });
-      setNestedValue(testObj, 'metadata.creator.creatorFirstName', 'Aino');
-      setNestedValue(testObj, 'metadata.creator.creatorLastName', 'Korhonen');
-      expect(testObj.metadata.creator).toEqual({
-        creatorFirstName: 'Aino',
-        creatorLastName: 'Korhonen',
+      setNestedValue(sampleRecord, 'metadata.dataLevel.dataLevelCode', '3');
+      setNestedValue(sampleRecord, 'metadata.dataLevel.dataLevelURI', 'thirduri');
+      expect(sampleRecord.metadata.dataLevel).toEqual({
+        dataLevelCode: '3',
+        dataLevelURI: 'thirduri',
       });
     });
 
-    it('should replace values from a nested array', () => {
-      setNestedValue(testObj, 'metadata.descriptions', [{ type: 'different type', value: 'new value' }]);
-      expect(testObj.metadata.descriptions).toEqual([{ type: 'different type', value: 'new value' }]);
-      setNestedValue(testObj, 'metadata.descriptions[0]', {
+    it('should replace values from a nested array of structs', () => {
+      setNestedValue(sampleRecord, 'metadata.creators', [
+        {
+          creatorGivenName: 'Tommi',
+          creatorFamilyName: 'Ranskalainen',
+        },
+      ]);
+      expect(sampleRecord.metadata.creators).toEqual([
+        {
+          creatorGivenName: 'Tommi',
+          creatorFamilyName: 'Ranskalainen',
+        },
+      ]);
+      setNestedValue(sampleRecord, 'metadata.creators[0].creatorGivenName', 'Aino');
+      setNestedValue(sampleRecord, 'metadata.creators[0].creatorFamilyName', 'Korhonen');
+      expect(sampleRecord.metadata.creators).toEqual([
+        {
+          creatorGivenName: 'Aino',
+          creatorFamilyName: 'Korhonen',
+        },
+      ]);
+
+      setNestedValue(sampleRecord, 'metadata.descriptions', [
+        { descriptionType: 'different type', descriptionText: 'new value' },
+      ]);
+      expect(sampleRecord.metadata.descriptions).toEqual([
+        { descriptionType: 'different type', descriptionText: 'new value' },
+      ]);
+      setNestedValue(sampleRecord, 'metadata.descriptions[0]', {
         type: 'aaaaa',
         value: 'bbbb.',
       });
-      expect(testObj.metadata.descriptions[0]).toEqual({
+      expect(sampleRecord.metadata.descriptions).toBeDefined();
+      expect(sampleRecord.metadata.descriptions![0]).toEqual({
         type: 'aaaaa',
         value: 'bbbb.',
       });
-      setNestedValue(testObj, 'metadata.descriptions[0].value', 'first description value');
-      expect(testObj.metadata.descriptions[0].value).toBe('first description value');
-      setNestedValue(testObj, 'metadata.descriptions[0].type', 'first description type');
-      expect(testObj.metadata.descriptions[0].type).toBe('first description type');
+      setNestedValue(sampleRecord, 'metadata.descriptions[0].descriptionText', 'first description value');
+      expect(sampleRecord.metadata.descriptions![0].descriptionText).toBe('first description value');
+      setNestedValue(sampleRecord, 'metadata.descriptions[0].descriptionType', 'first description type');
+      expect(sampleRecord.metadata.descriptions![0].descriptionType).toBe('first description type');
+    });
+
+    describe('appendValue', () => {
+      it('should append a value to an existing simple array', () => {
+        appendValue(sampleRecord, 'metadata.taxonomicCoverages', 'fourth');
+        expect(sampleRecord.metadata.taxonomicCoverages).toEqual(['first', 'second', 'third', 'fourth']);
+      });
+
+      it('should create a new array if the property does not exist', () => {
+        expect(sampleRecord.metadata.keywords).toBeUndefined();
+        appendValue(sampleRecord, 'metadata.keywords', 'new-item');
+        expect(sampleRecord.metadata.keywords).toEqual(['new-item']);
+      });
+
+      it('should overwrite already defined non-array property with a new array containing the value', () => {
+        expect(sampleRecord.metadata.assetType).toEqual('DOI');
+        appendValue(sampleRecord, 'metadata.assetType', 'value appended to not-array like property');
+        expect(sampleRecord.metadata.assetType).toEqual(['value appended to not-array like property']);
+      });
+
+      it('should not add undefined non-array property with a new array containing the value', () => {
+        appendValue(sampleRecord, 'pids', 'value appended to undefined property');
+        expect(sampleRecord.pids).toEqual(['value appended to undefined property']);
+      });
     });
   });
 });
