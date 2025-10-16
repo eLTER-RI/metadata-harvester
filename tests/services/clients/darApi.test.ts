cd77745feed71777d9d207353dc232e7b4c33b6b
Anna Vítová <anickavitova@seznam.cz>
2025-10-16 10:19:30 +0200

    tests: finish darApi mocked unit tests

--- tests/services/clients/darApi.test.ts
+++ tests/services/clients/darApi.test.ts
@@ -1,4 +1,4 @@
-import { postToDar, putToDar } from '../../../src/services/clients/darApi';
+import { findDarRecordBySourceURL, postToDar, putToDar } from '../../../src/services/clients/darApi';
 import { RecordDao } from '../../../src/store/dao/recordDao';
 import { CommonDataset } from '../../../src/store/commonStructure';
 import { log } from '../../../src/services/serviceLogging';
@@ -75,6 +75,103 @@ describe('DAR API Tests', () => {
       expect(result).toBeNull();
       expect(mockRecordDao.updateStatus).toHaveBeenCalledWith(sourceUrl, { status: 'failed' });
       expect(mockLog).toHaveBeenCalledWith('error', expect.stringContaining(`failed with : 400: ${errorResponse}`));
+    });
+  });
+
+  describe('putToDar', () => {
+    const darId = 'dar-record-id';
+    it('should PUT a new record and on success set record status to success and return null', async () => {
+      mockFetch.mockResolvedValueOnce({
+        ok: true,
+      });
+
+      await putToDar(darId, mockRecordDao, sourceUrl, mockDataset);
+
+      expect(mockFetch).toHaveBeenCalledWith(expect.any(String), {
+        method: 'PUT',
+        headers: expect.any(Object),
+        body: JSON.stringify(mockDataset, null, 2),
+      });
+      // called only to set to failure
+      expect(mockRecordDao.updateStatus).toHaveBeenCalledWith(sourceUrl, { status: 'success' });
+    });
+
+    it('should set record status to failed on empty apiResponse', async () => {
+      mockFetch.mockResolvedValueOnce(null);
+
+      await putToDar(darId, mockRecordDao, sourceUrl, mockDataset);
+
+      expect(mockRecordDao.updateStatus).toHaveBeenCalledWith(sourceUrl, { status: 'failed' });
+      expect(mockLog).toHaveBeenCalledWith(
+        'error',
+        expect.stringContaining(`PUT request ${sourceUrl} into dar failed`),
+      );
+    });
+
+    it('should set record status to failed if response not ok', async () => {
+      const errorResponse = 'Bad Request';
+      mockFetch.mockResolvedValueOnce({
+        ok: false,
+        status: 400,
+        text: () => Promise.resolve(errorResponse),
+      });
+
+      await putToDar(darId, mockRecordDao, sourceUrl, mockDataset);
+
+      expect(mockRecordDao.updateStatus).toHaveBeenCalledWith(sourceUrl, { status: 'failed' });
+      expect(mockLog).toHaveBeenCalledWith('error', expect.stringContaining(`failed with : 400: ${errorResponse}`));
+    });
+  });
+
+  describe('findDarRecordBySourceURL', () => {
+    it('should return null for incorrect API response', async () => {
+      mockFetch.mockResolvedValueOnce({
+        ok: true,
+        json: () =>
+          Promise.resolve({
+            // no 'hits'
+            data: [],
+          }),
+      });
+
+      const result = await findDarRecordBySourceURL(sourceUrl);
+
+      expect(result).toBeNull();
+    });
+
+    it('should return null when no records are found', async () => {
+      mockFetch.mockResolvedValueOnce({
+        ok: true,
+        json: () =>
+          Promise.resolve({
+            hits: {
+              hits: [],
+            },
+          }),
+      });
+
+      const result = await findDarRecordBySourceURL(sourceUrl);
+
+      expect(result).toBeNull();
+    });
+
+    it('should return the DAR ID when a record is found', async () => {
+      const foundDarId = 'dar-id-2000';
+      mockFetch.mockResolvedValueOnce({
+        ok: true,
+        json: () =>
+          Promise.resolve({
+            hits: {
+              hits: [{ id: foundDarId }],
+            },
+          }),
+      });
+
+      const result = await findDarRecordBySourceURL(sourceUrl);
+
+      expect(result).toBe(foundDarId);
+      const expectedEncodedUrl = encodeURIComponent(sourceUrl);
+      expect(mockFetch).toHaveBeenCalledWith(expect.stringContaining(expectedEncodedUrl), expect.any(Object));
     });
   });
 });
