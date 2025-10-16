1b2b5cd33ab58a62aec12ce7eda9549f69a62a5b
Anna Vítová <anickavitova@seznam.cz>
2025-10-16 14:15:52 +0200

    tests: finish tests for all cases of localDarSync

--- tests/services/jobs/syncWithDbRemote/localDarSync.test.ts
+++ tests/services/jobs/syncWithDbRemote/localDarSync.test.ts
@@ -1,7 +1,6 @@
 import { Pool } from 'pg';
 import { syncWithDar } from '../../../../src/services/jobs/syncDbWithRemote/localDarSync';
 import { deleteDarRecordsByIds, fetchDarRecordsByRepository } from '../../../../src/services/clients/darApi';
-import { RecordDao } from '../../../../src/store/dao/recordDao';
 import { log } from '../../../../src/services/serviceLogging';
 import { RepositoryType } from '../../../../src/store/commonStructure';
 
@@ -12,11 +11,19 @@ jest.mock('../../../../src/services/clients/darApi', () => ({
 
 jest.mock('../../../../src/store/dao/recordDao');
 jest.mock('../../../../src/services/serviceLogging');
-
+jest.mock('../../../../src/store/dao/recordDao', () => {
+  return {
+    RecordDao: jest.fn().mockImplementation(() => {
+      return {
+        listRepositoryDarIds: mockListRepositoryDarIds,
+      };
+    }),
+  };
+});
 const mockFetchDarRecords = fetchDarRecordsByRepository as jest.Mock;
 const mockDeleteDarRecords = deleteDarRecordsByIds as jest.Mock;
 const mockLog = log as jest.Mock;
-const MockedRecordDao = RecordDao as jest.Mocked<typeof RecordDao>;
+const mockListRepositoryDarIds = jest.fn();
 
 describe('syncWithDar', () => {
   let mockPool: Pool;
@@ -33,7 +40,52 @@ describe('syncWithDar', () => {
 
     expect(mockLog).toHaveBeenCalledWith('info', 'Error fetching DAR IDs: Error: Network Error');
 
-    expect(MockedRecordDao.prototype.listRepositoryDarIds).not.toHaveBeenCalled();
+    expect(mockListRepositoryDarIds).not.toHaveBeenCalled();
     expect(mockDeleteDarRecords).not.toHaveBeenCalled();
+  });
+
+  it('should log empty arrays and not delete if local and remote records in sync', async () => {
+    const identicalIds = ['id1', 'id2', 'id3'];
+    mockFetchDarRecords.mockResolvedValue(identicalIds);
+    mockListRepositoryDarIds.mockResolvedValue(identicalIds);
+
+    await syncWithDar('ZENODO' as RepositoryType, mockPool, true);
+
+    expect(mockLog).toHaveBeenCalledWith('warn', 'Extra on remote: []');
+    expect(mockLog).toHaveBeenCalledWith('warn', 'Extra in local: []');
+
+    expect(mockDeleteDarRecords).not.toHaveBeenCalled();
+  });
+
+  it('should log diffs in records but not delete them when darCleanup false', async () => {
+    const remoteIds = ['id1', 'id2', 'extra-remote'];
+    const localIds = ['id1', 'id2', 'extra-local'];
+    mockFetchDarRecords.mockResolvedValue(remoteIds);
+    mockListRepositoryDarIds.mockResolvedValue(localIds);
+
+    await syncWithDar('ZENODO' as RepositoryType, mockPool, false);
+
+    expect(mockFetchDarRecords).toHaveBeenCalledWith('ZENODO');
+    expect(mockListRepositoryDarIds).toHaveBeenCalledWith('ZENODO');
+
+    expect(mockLog).toHaveBeenCalledWith('warn', 'Extra on remote: [\n  "extra-remote"\n]');
+    expect(mockLog).toHaveBeenCalledWith('warn', 'Extra in local: [\n  "extra-local"\n]');
+
+    expect(mockDeleteDarRecords).not.toHaveBeenCalled();
+  });
+
+  it('should delete extra records from dar when darCleanup is true', async () => {
+    const remoteIds = ['id1', 'id2', 'extra-remote'];
+    const localIds = ['id1', 'id2', 'extra-local'];
+    mockFetchDarRecords.mockResolvedValue(remoteIds);
+    mockListRepositoryDarIds.mockResolvedValue(localIds);
+
+    await syncWithDar('ZENODO' as RepositoryType, mockPool, true);
+
+    expect(mockLog).toHaveBeenCalledWith('warn', 'Extra on remote: [\n  "extra-remote"\n]');
+    expect(mockLog).toHaveBeenCalledWith('warn', 'Extra in local: [\n  "extra-local"\n]');
+    expect(mockLog).toHaveBeenCalledWith('warn', 'Deleting all extra resources from Dar.');
+    expect(mockDeleteDarRecords).toHaveBeenCalledTimes(1);
+    expect(mockDeleteDarRecords).toHaveBeenCalledWith(['extra-remote']);
   });
 });
