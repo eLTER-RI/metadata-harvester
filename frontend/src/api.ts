dad82b81bf78702ed39939f8d5172628c3e6873e
Anna Vítová <anickavitova@seznam.cz>
2025-10-20 17:08:41 +0200

    fix: remove .data extraction from central axios

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,8 +3,3 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'http://localhost:3000/api',
 });
-
-api.interceptors.response.use(
-  (response) => response.data,
-  (error) => Promise.reject(error),
-);
