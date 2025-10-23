fb4eff98495d0acc749bdf531fa144121a85c380
Anna Vítová <anickavitova@seznam.cz>
2025-10-23 19:31:43 +0200

    frontend: add projects group fields

---
+++ frontend/src/components/metadataForm/groups/ProjectsGroup.tsx
@@ -0,0 +1,28 @@
+import { Form, Button, Segment, Header, Icon } from 'semantic-ui-react';
+
+export const ProjectsGroup = () => {
+  return (
+    <Segment>
+      <Header as="h3">
+        <Icon name="folder" />
+        Projects
+        <Header.Subheader>Projects associated with the dataset</Header.Subheader>
+      </Header>
+
+      <Segment style={{ marginBottom: '1rem' }}>
+        <Form.Group widths="equal">
+          <Form.Field>
+            <label>Project Name *</label>
+            <Form.Input placeholder="Name of the project" />
+          </Form.Field>
+          <Form.Field>
+            <label>Project ID</label>
+            <Form.Input placeholder="Project identifier" />
+          </Form.Field>
+        </Form.Group>
+      </Segment>
+
+      <Button type="button" icon="plus" content="Add Project" style={{ marginTop: '1rem' }} />
+    </Segment>
+  );
+};
