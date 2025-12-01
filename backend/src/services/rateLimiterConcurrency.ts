d8d32239b0ccc9ddb2645270fa55a9da4f4dab4e
Anna Vítová <anickavitova@seznam.cz>
2025-12-01 19:34:21 +0100

    feat: add helper function for rateLimiterConcurrency

--- backend/src/services/rateLimiterConcurrency.ts
+++ backend/src/services/rateLimiterConcurrency.ts
@@ -1,23 +1,18 @@
 import Bottleneck from 'bottleneck';
 
-// How long to wait after launching a job before launching another one.
+function createRateLimiter(requestsPerMinute: number): Bottleneck {
+  const minTimeMs = 60000 / requestsPerMinute;
+  return new Bottleneck({
+    minTime: minTimeMs,
+  });
+}
 
-export const b2shareLimiter = new Bottleneck({
-  minTime: 10000 / 80, // 60 requests per minute = 750ms between requests
-});
+export const b2shareLimiter = createRateLimiter(80); // 40 requests per minute = 750ms between requests
 
-export const zenodoLimiter = new Bottleneck({
-  minTime: 60000 / 80, // 80 requests per minute = 750ms between requests
-});
+export const zenodoLimiter = createRateLimiter(80); // 80 requests per minute = 750ms between requests
 
-export const fieldSitesLimiter = new Bottleneck({
-  minTime: 60000 / 80, // 80 requests per minute = 750ms between requests
-});
+export const fieldSitesLimiter = createRateLimiter(80); // 80 requests per minute = 750ms between requests
 
-export const deimsLimiter = new Bottleneck({
-  minTime: 60000 / 80, // 80 requests per minute = 750ms between requests
-});
+export const deimsLimiter = createRateLimiter(80); // 80 requests per minute = 750ms between requests
 
-export const darLimiter = new Bottleneck({
-  minTime: 60000 / 60, // 1000 ms between requests (1 per second)
-});
+export const darLimiter = createRateLimiter(60); // 60 requests per minute = 1000ms between requests (1 per second)
