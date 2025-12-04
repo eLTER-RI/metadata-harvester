371a50229b3f330d95d9aaf9a976bf1475171039
Anna Vítová <anickavitova@seznam.cz>
2025-12-04 10:58:06 +0100

    frontend: fix linting issues ignored previously

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'semantic-ui-css/semantic.min.css';
-import App from './App.jsx'
+import App from './App.jsx';
 
 const queryClient = new QueryClient();
 
@@ -13,4 +13,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </QueryClientProvider>
   </StrictMode>,
-)
+);
